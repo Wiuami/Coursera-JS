934289a91dff73b0279a92a367748017d3133456
PeterDang <picachunam32@gmail.com>
2025-03-02 15:05:22 +0700

    CourseraJS_PhotoGallery

---
+++ gallery.js
@@ -0,0 +1,11 @@
+function upDate(previewPic) {
+    var imageDiv = document.getElementById('image');
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+    imageDiv.textContent = previewPic.alt;
+}
+
+function unDo() {
+    var imageDiv = document.getElementById('image');
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.textContent = "Hover over an image below to display here.";
+}
